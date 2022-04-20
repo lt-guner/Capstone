64f152a3af12092f3fa919551a861863a517e4cd
Timur Guner <timur.ozdemirali.guner@gmail.com>
2022-04-19 21:29:15 -0700

    A new update based on josh's recommendations

--- Chess/move.py
+++ Chess/move.py
@@ -1,3 +1,6 @@
+from constants import *
+
+
 class Move:
     """
     The init takes the first clicked square and second clicked square and stores the starting and ending coords.
@@ -11,13 +14,9 @@ class Move:
         self.end_col = endSq[1]
         self.piece_moved = board[self.start_row][self.start_col]
         self.piece_captured = board[self.end_row][self.end_col]
+
         # creates a unique id like a hash function
         self.move_id = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
-        self.pieces = {'wP': "White Pawn", 'wR': 'White Rook', 'wN': 'White Knight', 'wB': 'White Bishop',
-                       'wQ': 'White Queen', 'wK': 'White King', 'bP': "Black Pawn", 'bR': 'Black Rook',
-                       'bN': 'Black Knight', 'bB': 'Black Bishop', 'bQ': 'Black Queen', 'bK': 'Black King'}
-        self.columns_chess = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H'}
-        self.rows_chess = {0: '8', 1: '7', 2: '6', 3: '5', 4: '4', 5: '3', 6: '2', 7: '1'}
 
     def __eq__(self, other):
         """
@@ -29,30 +28,41 @@ class Move:
 
     def get_chess_notation(self):
         """
-        Returns the chess notation of the move like E4 E5 which is moving a piece from E4 to E5
+        Returns a string of a move's start and end square in chess notation e.g. 'E2 E4'
+        To be sent to the server in multiplayer to update opponent
         """
-
-        return str(self.columns_chess[self.start_col]) + str(self.rows_chess[self.start_row]) + " " + \
-            str(self.columns_chess[self.end_col]) + str(self.rows_chess[self.end_row])
+        string = COL_TO_NOTATION[self.start_col] + ROW_TO_NOTATION[self.start_row]  # start square
+        string += ''
+        string += COL_TO_NOTATION[self.end_col] + ROW_TO_NOTATION[self.end_row]     # end square
+        return string
 
     def get_move_legible(self):
         """
         Returns a human legible move to be used when logging the move in the UI
         """
+        # ID: piece Moves From chess n to square
+        string = str(self.move_id) + ': '
+        string += PIECES[self.piece_moved]
+        string += ' Moves From '
+        string += COL_TO_NOTATION[self.start_col] + ROW_TO_NOTATION[self.start_row]
+        string += ' to '
+        string += COL_TO_NOTATION[self.end_col] + ROW_TO_NOTATION[self.end_row]
 
-        if self.piece_captured == '--':
-            return str(self.move_id) + ': ' + self.pieces[self.piece_moved] + ' Moves From ' + \
-                   str(self.columns_chess[self.start_col]) + str(self.rows_chess[self.start_row]) + ' to ' + \
-                   str(self.columns_chess[self.end_col]) + str(self.rows_chess[self.end_row])
-        else:
-            return str(self.move_id) + ': ' + self.pieces[self.piece_moved] + ' Moves From ' + \
-                   str(self.columns_chess[self.start_col]) + str(self.rows_chess[self.start_row]) + ' to ' + \
-                   str(self.columns_chess[self.end_col]) + str(self.rows_chess[self.end_row]) + ' Capturing ' + \
-                   self.pieces[self.piece_captured]
+        # if piece was captured, add it to the string
+        if self.piece_captured:
+            string += ' Capturing '
+            string += PIECES[self.piece_captured]
+
+        return string
+
+    def get_piece_moved(self):
+        """
+        Returns the piece that moved
+        """
+        return self.piece_moved
 
     def get_piece_captured(self):
         """
         Returns the name of the captured piece
         """
-
-        return self.pieces[self.piece_captured]
+        return self.piece_captured
