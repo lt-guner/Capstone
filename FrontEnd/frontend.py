8bc2e4fa5a7c28c662aacb45fe08410bc7a00dcf
Joshua Luo <luojo@oregonstate.edu>
2022-04-22 21:26:56 -0400

    added pseudo code
    pseudo code describes:
    - variable to determine whether it's this player's turn
    - integration with client and game engine for validating move and storing in data buffer for sending to server
    - checking data buffer for opponent move to update game engine

--- FrontEnd/frontend.py
+++ FrontEnd/frontend.py
@@ -14,6 +14,9 @@ WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 
 pygame.display.set_caption('Chess')
 
+# variable to store player color (get from client upon connection)
+# use player color and engine.white_turn to determine if it's the player's turn
+
 def main():
     run = True
     clock = pygame.time.Clock()
@@ -28,15 +31,29 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+            # if user's turn:
+            # (indent code below when if statement is implemented)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if board.selected_piece:
                     move = Move(board.selected_piece, mouse_square, engine.board)
+                    # call game engine (piece)_move() to get list of valid moves for this piece and location
+                    # if move is in valid_moves_list
+                        # place move into data buffer shared with client to read and send to server
+                        # engine.make_move(move) --- below
+                        # board.selected_piece = None --- below
+                    # else nothing or deselect piece (place piece back)
+
                     engine.make_move(move)
                     board.selected_piece = None
                 else:
                     row, col = mouse_square
                     if not engine.is_empty_square(row, col):
                         board.selected_piece = mouse_square
+            # (end indent)
+            # else: opponent turn:
+                # if opponent move data buffer is not None:
+                    # create Move object from opponent move data
+                    # engine.make_move(opponent Move object)
 
         board.draw_squares(WIN)
         board.draw_selected(WIN)
