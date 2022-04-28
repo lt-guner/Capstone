924dce3562f080aa4b4c7c5c4a32ca51384d60f6
Joshua Luo <luojo@oregonstate.edu>
2022-04-27 21:06:50 -0400

    Revert "initial integration with client network"
    This reverts commit 1d6b93db0d4021a94ebdecf553da84eaceb63e80.

--- FrontEnd/frontend.py
+++ FrontEnd/frontend.py
@@ -1,7 +1,7 @@
 import pygame
 
-from constants import *
-from board import Board
+from .constants import WIDTH, HEIGHT
+from .board import Board
 
 from Chess.chess_engine import ChessEngine
 from Chess.move import Move
@@ -13,100 +13,6 @@ FPS = 60
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 
 pygame.display.set_caption('Chess')
-
-##### integrating client's socket connection code (Josh)  #####
-
-from server_files.client_network import Network
-
-# buffer to store move data
-make_move = None        # this client has made a move to send to opponent
-opponent_move = None    # received move made by opponent
-
-def get_player_color(data):
-    if connect_message[-1] == '0':
-        return WHITE
-    else:
-        return BLACK
-
-def is_turn():
-    if player_color == WHITE and engine.white_turn:
-        return True
-    if player_color == BLACK and not engine.white_not:
-        return True
-    return False
-
-n = Network()
-
-# establish connection and receive first
-connect_message = n.connect()
-if connect_message == ERROR:
-    print(GAME_FULL)
-else:
-    print(connect_message)
-    player_color = get_player_color(connect_message)
-
-    # initial message: waiting for server to indicate game is ready
-    message = WAITING_GAME_START
-    n.send(message)
-
-def communicate_server(net_conn: Network):
-    try:
-        reply = net_conn.receive()
-
-        # opponent disconnected. stay ready, waiting for opponent to reconnect
-        if reply == OPPONENT_DISCONNECTED:
-            payload = READY
-
-        # server is waiting for an opponent to connect
-        elif reply == WAITING_FOR_OPPONENT:
-            # continue to wait
-            payload = WAITING_GAME_START
-
-        # server is waiting for a move
-        elif reply == WAITING_FOR_TURN:
-            # this client's turn
-            if is_turn():
-                global make_move
-                # no move data to send
-                if make_move is None:
-                    payload = READY
-
-                # has move data to send
-                else:
-                    print("SENDING MOVE TO SERVER")
-                    # ready move data, empty move data buffer, end turn
-                    payload = make_move
-                    make_move = None
-
-            # not this client's turn
-            else:
-                global opponent_move
-                # no move data received from opponent
-                if opponent_move is None:
-                    # stay ready
-                    payload = READY
-
-                # received move data from opponent
-                else:
-                    # store opponent move data into buffer
-                    opponent_move = reply
-                    opponent_move = None
-
-                    # update game state in game engine and UI
-                    print("RECEIVED OPPONENT MOVE FROM SERVER")
-
-                    # start turn, send ready message
-                    payload = READY
-
-        net_conn.send(payload)
-
-        print('Sending to server:', payload)
-        print('Received from server:', reply)
-
-    except:
-        pass
-
-##### end integration code #####
 
 # variable to store player color (get from client upon connection)
 # use player color and engine.white_turn to determine if it's the player's turn
@@ -150,10 +56,6 @@ def main():
                 # if opponent move data buffer is not None:
                     # create Move object from opponent move data
                     # engine.make_move(opponent Move object)
-
-        ##### integration code for sending/receiving data with server (Josh) #####
-        communicate_server(n)
-        ##### end integration code #####
 
         board.draw_squares(WIN)
         board.draw_selected(WIN)
